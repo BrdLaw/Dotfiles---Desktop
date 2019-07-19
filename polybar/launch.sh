8cf4bd86ffaa9b103c2521cc88fa1a818a600f7f
BrdLaw <52295123+BrdLaw@users.noreply.github.com>
2019-07-19 15:25:01 -0500

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep  -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+#polybar example1 &
+#polybar example2 &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+echo "Bars launched..."
